abdb0fe6f237dc4bd16543a29b603fd2d69fbcbd
KKamji <xowl5460@naver.com>
2024-07-20 05:24:29 +0900

    feat: create frontend and images bucket

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-exam-master"
-    key            = "iam_group_membership/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "terraform-state-weasel"
+    key     = "persistent/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
