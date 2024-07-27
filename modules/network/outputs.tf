6bf43606698a47e34beeb084eef4c6c8c957332c
War-Oxi <xowl5460@naver.com>
2024-07-27 20:09:13 +0900

    feat: create NAT Gateway

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -23,6 +23,11 @@ output "internet_gateway_id" {
   value       = aws_internet_gateway.this.id
 }
 
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway"
+  value       = aws_nat_gateway.this.id
+}
+
 output "public_route_table_id" {
   description = "The ID of the public route table."
   value       = aws_route_table.public.id
