697bcf542a179debf40f2383b861866f3c254f06
ChoiSeungBeom <csbk1998@naver.com>
2024-07-28 18:04:22 +0900

    수정사항 재반영

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,6 +1,6 @@
 #rds subnet group 생성
 resource "aws_db_subnet_group" "this" {
-  name       = var.name
+  name       = var.subnet_group_name
   subnet_ids = var.subnet_ids
 }
 
@@ -9,14 +9,15 @@ resource "aws_db_instance" "this" {
   identifier             = var.identifier
   allocated_storage      = 10
   engine                 = "mysql"
-  instance_class         = "db.t3.micro"
+  instance_class         = "db.t4g.micro"
   username               = local.rds_username
   password               = local.rds_password
   availability_zone      = var.availability_zone
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = var.vpc_security_group_ids
   skip_final_snapshot    = var.skip_final_snapshot
-  publicly_accessible    = false
+  publicly_accessible    = true
+
 }
 
 data "aws_secretsmanager_secret" "rds_login" {
